8d839263cdc27317c95652e9957a29a830b0f305
Eduardo Henrique Kenji Shibukawa <eduardoshibuka@gmail.com>
2017-08-05 01:09:45 -0300

    Fazendo classe de inviduo

--- p_medianas_capacitada.py
+++ p_medianas_capacitada.py
@@ -7,12 +7,16 @@ class Vertice:
         self.capacidade = capacidade
 
 class Mediana:
-    def __init__(self, vertice):
-        self.vertice = vertice        
-        self.conjunto = set([])
-        self.__distancias = {}                
+    def __init__(self, vertice, conjunto = set([])):
+        self.vertice = vertice
+        self.conjunto = conjunto
+        self.__distancias = {}
         self.__demanda = 0
-        self.__distancia_total = 0
+        self.__distancia_total = 0        
+        if len(conjunto) != 0:                    
+            for v in conjunto:
+                self.__demanda += v.demanda
+                self.__distancia_total += self.distancia(self.vertice, v)
 
     def adicionar_vertice(self, v):
         self.conjunto.add(v)
@@ -30,21 +34,42 @@ class Mediana:
         if not aresta in self.__distancias:
             xa, ya = v1.coordenada
             xb, yb = v2.coordenada
-            self.__distancias[aresta] = math.sqrt(math.pow((xa - xb), 2) + math.pow((ya - yb), 2))        
+            self.__distancias[aresta] = math.sqrt(math.pow((xa - xb), 2) + math.pow((ya - yb), 2))
         return self.__distancias[aresta]
 
-    def somar_distancia(self):                
-        return self.__distancia_total       
-        
-    def cardinalidade(self):
-        return len(self.conjunto)
+    def distancia_total(self):
+        return self.__distancia_total
+
+class Individuo:
+    def __init__(self, medianas = []):
+        self.medianas = medianas        
+
+    def fitness(self):        
+        aptidao = 0
+        for mediana in self.medianas:
+            aptidao += mediana.distancia_total
+
+        return aptidao
 
 class AlgoritmoGenetico:
     def __init__(self, vertices):
-        self.medianas = []
+        self.medianas = set([])
         self.vertices = vertices
         self.valor_solucao = 0
 
+    def gerar_nova_mediana(self, vertice, mediana):
+        novo_conjunto = mediana.conjunto.discard(vertice)
+        novo_conjunto.add(mediana.vertice)
+
+        nova_mediana = Mediana(vertice)        
+        for v in novo_conjunto:
+            if not nova_mediana.capacidade(v):
+                return None
+
+            nova_mediana.adicionar_vertice(v)                            
+
+        return nova_mediana
+    
     def gerar_populacao_inicial(self):
         pass
 
@@ -52,7 +77,9 @@ class AlgoritmoGenetico:
         return valor < self.valor_solucao
 
     def solucionar(self):
+        self.gerar_populacao_inicial()
         pass
+                
 
 if (__name__ == "__main__"):
     m1 = Mediana(Vertice((1, 1), 120, 1))
@@ -65,10 +92,10 @@ if (__name__ == "__main__"):
     m2.adicionar_vertice(Vertice((4, 1), 120, 8))
             
     print(m1.vertice.coordenada)
-    print(m1.somar_distancia())
+    print(m1.distancia_total())
     print(m1.demanda_atual())
     print(m2.vertice.coordenada)
-    print(m2.somar_distancia())
+    print(m2.distancia_total())
     print(m2.demanda_atual())
 
     medianas = [m1, m2]
